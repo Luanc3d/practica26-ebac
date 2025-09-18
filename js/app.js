9bf3cf66275c97a6ecb61c5518e552deb3a0e092
Luanc3d <luancrsa@gmail.com>
2025-09-18 09:53:44 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function calcularEdad(fechaNacimiento) {
+    var fechaActual = new Date();
+    var fechaNac = new Date(fechaNacimiento);
+    var añoActual = fechaActual.getFullYear();
+    var mesActual = fechaActual.getMonth();
+    var diaActual = fechaActual.getDate();
+    var añoNac = fechaNac.getFullYear();
+    var mesNac = fechaNac.getMonth();
+    var diaNac = fechaNac.getDate();
+    var edad = añoActual - añoNac;
+    var cumpleañosYaPasó = mesActual > mesNac || (mesActual === mesNac && diaActual >= diaNac);
+    if (!cumpleañosYaPasó) {
+        edad--;
+    }
+    return edad;
+}
+var nombre = "Luis";
+var fecha = "1982-09-15";
+var edad = calcularEdad(fecha);
+console.log("".concat(nombre, " tiene ").concat(edad, " a\u00F1os."));
---
+++ js/app.ts
@@ -0,0 +1,30 @@
+function calcularEdad(fechaNacimiento: string): number {
+  const fechaActual: Date = new Date();
+  const fechaNac: Date = new Date(fechaNacimiento);
+
+  const añoActual: number = fechaActual.getFullYear();
+  const mesActual: number = fechaActual.getMonth();
+  const diaActual: number = fechaActual.getDate();
+
+  const añoNac: number = fechaNac.getFullYear();
+  const mesNac: number = fechaNac.getMonth();
+  const diaNac: number = fechaNac.getDate();
+
+  let edad: number = añoActual - añoNac;
+
+  const cumpleañosYaPasó: boolean =
+    mesActual > mesNac || (mesActual === mesNac && diaActual >= diaNac);
+
+  if (!cumpleañosYaPasó) {
+    edad--;
+  }
+
+  return edad;
+}
+
+
+const nombre: string = "Luis";
+const fecha: string = "1982-09-15";
+const edad: number = calcularEdad(fecha);
+
+console.log(`${nombre} tiene ${edad} años.`);
